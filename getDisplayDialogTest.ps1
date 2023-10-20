11e9cf38e99e1bbfd61c928aae1adae517ac22c3
John C. Welch <jwelch@bynkii.com>
2023-10-19 20:50:51 -0500

    created initial parameter array, will have to test a LOT

---
+++ getDisplayDialogTest.ps1
@@ -0,0 +1,22 @@
+function getDisplayDialog() {
+     Param (
+          [Parameter(Mandatory = $true)] [string] $dialogText,
+          [Parameter(Mandatory = $false)] [bool] $hiddenAnswer,
+          [Parameter(Mandatory = $false)] [array] $buttons,
+          [Parameter(Mandatory = $false)] [string] $defaultButtonText,
+          [Parameter(Mandatory = $false)] [int] $defaultButtonInt,
+          [Parameter(Mandatory = $false)] [string] $cancelButtonText,
+          [Parameter(Mandatory = $false)] [int] $cancelButtonInt,
+          [Parameter(Mandatory = $false)] [string] $title,
+          [Parameter(Mandatory = $false)] [string] $iconText,
+          [Parameter(Mandatory = $false)] [int] $iconInt,
+          [Parameter(Mandatory = $false)] [string] $iconEnum = "caution",
+          [Parameter(Mandatory = $false)] [string] $iconPath,
+          [Parameter(Mandatory = $false)] [int] $givingUpAfter 
+     )
+
+     Write-host "Dialog text is: $dialogText"
+}
+
+$dialogText = "Test dialog text"
+getDisplayDialog 
